aee647ba365364212c04a2ba57eac8b309e82a3d
MuriloJrMarques <muriloj2702@gmail.com>
2026-02-08 23:08:09 -0300

    subindo metodos no repositorio GetByID, Delete, List e Update.

--- internal/repository/transactionRepository.go
+++ internal/repository/transactionRepository.go
@@ -16,3 +16,19 @@ func NewTransactionRepository(q *db.Queries) *TransactionRepository {
 func (r *TransactionRepository) Create(ctx context.Context, params db.CreateTransactionParams) (db.Transaction, error) {
     return r.q.CreateTransaction(ctx, params)
 }
+
+func (r *TransactionRepository) GetByID(ctx context.Context, id int32) (db.Transaction, error) {
+	return r.q.GetTransaction(ctx, id)
+}
+
+func (r *TransactionRepository) Delete(ctx context.Context, id int32) error {
+	return r.q.DeleteTransaction(ctx, id)
+}
+
+func (r *TransactionRepository) List(ctx context.Context) ([]db.Transaction, error) {
+	return r.q.ListTransactions(ctx)
+}
+
+func (r *TransactionRepository) Update(ctx context.Context, params db.UpdateTransactionParams) (db.Transaction, error) {
+	return r.q.UpdateTransaction(ctx, params)
+}
